8e9e1a0d3829f60dfb045a54cffc6016b5534860
beebikun <beebikun@users.noreply.github.com>
2014-08-30 09:46:04 +1000

    Update example.py

--- example.py
+++ example.py
@@ -20,7 +20,7 @@ SETTINGS = {
     },
 }
 
-from libsms import sms_transport
+import libsms
 
 NAME = "dummy"
 PHONE = "123"
@@ -28,6 +28,23 @@ MSG = "qwerty"
 
 
 cfg = SETTINGS[NAME]
+
+class SmsTransportFactory(object):
+    """Фабрика для SmsTransport"""
+    def __init__(self):
+        pass
+
+    def get_transport(self, transport_name):
+        """Возвращает нужный класс"""
+        return getattr(libsms, transport_name)
+
+
+def sms_transport(BACKEND, PARAMS=None):
+    """Возвращает экземпляр нужного класса"""
+    transport = SmsTransportFactory().get_transport(BACKEND)
+    return transport(PARAMS)
+
+
 """
 С помощью функции sms_transport получаем экзмепляр нужного класса
 и вызываем у него метод send
