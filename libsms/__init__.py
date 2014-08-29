ff89aa192d26bcabc66fb866e82e10f02d314149
beebikun <beebikun@users.noreply.github.com>
2014-08-30 09:45:46 +1000

    Update __init__.py

--- libsms/__init__.py
+++ libsms/__init__.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-class SmsTransportFactory(object):
-    """Фабрика для SmsTransport"""
-    def __init__(self):
-        pass
-
-    def get_transport(self, transport_name):
-        """Возвращает нужный класс"""
-        return globals()[transport_name]
-
-
 class SmsTransport(object):
     def __init__(self, params=None):
         self.params = params
@@ -36,8 +25,3 @@ class SmsTransportGW1(SmsTransport):
 class SmsTransportGW2(SmsTransport):
     pass
 
-
-def sms_transport(BACKEND, PARAMS=None):
-    """Возвращает экземпляр нужного класса"""
-    transport = SmsTransportFactory().get_transport(BACKEND)
-    return transport(PARAMS)
